deedb2a0f5893fcc4c724293df5eb00a0ef54a7f
Ian <chemwenokibet11@gmail.com>
2022-11-24 10:04:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+
+//because number is 62 both numbers are 31 and 2
+
+let num1 = 31;
+let num2 = 2;
+let multiply = (num1 * num2);
+console.log(multiply);
+
+//the math.ceil rounds off the random number to the highest
+let random = Math.ceil(Math.random());
+
+
+//24 % 5 returns 4.8 math.round() rounds it to 5 math.trunc()truncates it to 4 math.floor()rounds numbers down
+
+const num3 = 24;
+const num4 = 5;
+let mod = Math.trunc(num3 % num4);
+console.log(mod)
+
+
+let max = Math.max(11, 12, 20)
